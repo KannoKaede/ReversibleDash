30e4af765d7a2c9b3568d0c36e131bcbff44fc2b
KannoKaede <oom2425142@stu.o-hara.ac.jp>
2025-09-26 15:09:36 +0900

    インゲームのステージ描画の基礎を作成

--- InGame.cpp
+++ InGame.cpp
@@ -2,3 +2,7 @@
 #include"InGame.h"
 bool isGameStop;
 int stageNumber;
+
+void DrawStage(int stageNum) {
+	DrawCone3D(VGet(30,70,0), VGet(30,0,0), 50, 16, GetColor(255, 0, 0), GetColor(255, 0, 0), TRUE);
+}
--- InGame.h
+++ InGame.h
@@ -7,4 +7,6 @@ extern bool isGameStop;
 extern int stageNumber;     // 現在のステージ番号
 const int MAX_STAGE_NUM = 7;    //ステージの最大数：配列で使用するためステージ数＋１
 const float goalPosition[MAX_STAGE_NUM] = { 0,500,100,100,100,100,100 };
+
+void DrawStage(int stageNum);
 #endif
